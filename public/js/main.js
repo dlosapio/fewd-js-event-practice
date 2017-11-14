a9441b1621adffc295d0382e66951755012dde9a
Dan Losapio <danlosapio@gmail.com>
2017-11-14 12:57:20 -0500

    mess with Javascript

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,48 @@
-(function () {
+const button1 = document.querySelector('main div div button')
+const button2 = document.getElementsByClassName('button-xl button-success')
+const button3 = document.querySelector('button-xl button-warning')
+const row = document.querySelector('row')
+let red = false;
 
-  // your js goes here
+//First behavior
 
-})();
+button1.addEventListener('click', firstBehavior, false)
+
+function firstBehavior() {
+		// console.log('hello');
+	if (!red) {
+		changeRed() 
+	} else {
+		changeBlue() 
+	}
+}
+
+function changeRed() {
+	red = true
+	row.classList.add('button-danger')
+	row.classList.remove('button-xl')
+}
+
+function changeBlue() {
+	red = false
+	row.classList.remove('button-xl')
+	row.classList.add('button-danger')
+}
+
+
+//Second behavior
+
+button2.addEventListener('click', secondBehavior, false)
+
+function secondBehavior() {
+	console.log('hi');
+}
+
+
+//Third behavior
+
+button3.addEventListener('click', thirdBehavior, false)
+
+function thirdBehavior() {
+	document.getElementsByClassName('button-xl button-success').innerHTML = "You Clicked Me";
+}
